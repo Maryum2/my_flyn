485e28bee53f5d0ed5cf72bfe003d271b30cb22f
Maryum <maryum@192.168.1.6>
2025-08-20 00:43:49 +0500

    changed location of some files

--- lib/controllers/home/home_controller.dart
+++ lib/controllers/home/home_controller.dart
@@ -1,8 +1,8 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  // Current tab index on the My Page counters (신청/진행/완료)
-  final currentStatusTab = 1.obs; // 0:신청,1:진행,2:완료
+  // Current tab index on the My Page counters 
+  final currentStatusTab = 1.obs; 
 
   void setTab(int index) => currentStatusTab.value = index;
 }
